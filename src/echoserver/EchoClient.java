532080c17a346dd68506cc03928d1d7840b440d7
Charles Menne <menne019@reliant.morris.umn.edu>
2017-10-26 11:52:44 -0500

    Created EchoServer and EchoClient
    need work on creating sockets correctly

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,34 @@
 package echoserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.*;
+import java.io.*;
+
 public class EchoClient {
+	 public static void main(String[] args) {
+		    try {
+		      Socket socket = new Socket("Reliant.morris.umn.edu", 6013);
+		      // You can change 127.0.0.1 to a machine name if you want to try this across
+		      // the network to another machine.
+		      // Socket socket = new Socket("some.machine.edu", 6013);
+		      
+		      InputStream input = socket.getInputStream();
+		      
+		      BufferedReader read = new BufferedReader(new InputStreamReader(input));
+		      
+		      byte in;
+		      while ((in = (byte) read.read()) != -1) {
+		        System.out.println(in);
+		      }
+		      
+		      socket.close();
+		    } catch (IOException ioe) {
+		      System.out.println("We caught an exception");
+		      System.err.println(ioe);
+		    }
+		  }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,32 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+  public static void main(String[] args) {
+    try {
+      ServerSocket sock = new ServerSocket(6013);
+      
+      while (true) {
+        
+        Socket client = sock.accept();
+        System.out.println("Got a request!");
+        
+        InputStream in = client.getInputStream();
+        OutputStream out = client.getOutputStream();
+        
+        PrintWriter writer = new PrintWriter(out, true);
+        byte b;
+        while ((b = (byte) in.read()) != -1) {
+        	writer.println(b);
+        }
+        
+        
+        client.close();
+      }
+    } catch (IOException ioe) {
+      System.err.println(ioe);
+    }
+  }
 }
